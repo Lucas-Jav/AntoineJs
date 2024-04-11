80e631612226380845016b6199a74d3e6d2045e5
Lucas-Jav <lucas.meialua@outlook.com>
2024-04-11 00:30:14 -0300

    :sparkles: feat

--- actions/create.js
+++ actions/create.js
@@ -1,51 +1,76 @@
 const { prompt } = require('enquirer');
-const { mkdirSync, chdir, writeFileSync } = require('../utils/fsUtils');
-const { initProject, installDependencies, configureBabel, createIndexFile, createConfigFile } = require('../utils/config.js');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
 
 const create = async (name) => {
     const response = await prompt([
-      {
-        type: 'confirm',
-        name: 'useTypescript',
-        message: 'Use TypeScript?',
-        initial: false,
-      },
+        {
+            type: 'confirm',
+            name: 'useTypescript',
+            message: 'Use TypeScript?',
+            initial: false,
+        },
     ]);
 
     const { useTypescript } = response;
 
     console.log(`Creating project '${name}'...`);
 
-    mkdirSync(name);
-    chdir(name);
+    fs.mkdirSync(name);
+    process.chdir(name);
 
+    // Estrutura de diretórios padrão
     const folders = [
-      'app',
-      'app/Http',
-      'app/Models',
-      'config',
-      'database',
-      'public',
-      'resources',
-      'seeders',
-      'resources/views',
-      'routes',
-      'storage',
-      'storage/images',
-      'storage/files',
+        'app',
+        'app/Http',
+        'app/Models',
+        'config',
+        'database',
+        'public',
+        'resources',
+        'seeders',
+        'resources/views',
+        'routes',
+        'storage',
+        'storage/images',
+        'storage/files',
     ];
 
     folders.forEach(folder => {
-      mkdirSync(folder, { recursive: true });
+        fs.mkdirSync(folder, { recursive: true });
     });
 
-    initProject();
-    installDependencies(useTypescript ? ['typescript'] : []);
-    configureBabel();
-    createIndexFile();
-    createConfigFile();
+    // Inicializar o projeto Node.js
+    execSync('npm init -y');
 
-    console.log('Project created successfully!');
+    // Instalar dependências
+    const dependencies = useTypescript ? ['typescript'] : [];
+    /* execSync(`npm install --save-dev @babel/core @babel/preset-env ${dependencies.join(' ')}`); */
+    execSync(`npm install --save-dev  ${dependencies.join(' ')}`);
+
+    // Configurar Babel
+    fs.writeFileSync('.babelrc', `{"presets": ["@babel/preset-env"]}`);
+
+    // Exemplo de arquivo index.js
+    fs.writeFileSync('index.js', '// Seu código aqui');
+
+    fs.writeFileSync('./config/config.js', 
+`module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "123456",
+    database: "your-database",
+    define: {
+        timestamps: true,
+    },
+};`)
+
+console.log("\n🔥 project created successfully 🔥");
+console.log(`\n$ cd ${name}`);
+console.log("$ npm install\n");
+
 };
 
 module.exports = { create };
--- antoine.js
+++ antoine.js
@@ -5,77 +5,12 @@ const { execSync } = require('child_process');
 const { program } = require('commander');
 
 const { prompt } = require('enquirer');
+const { create } = require('./actions/create');
 
 program
   .command('create <name>')
   .description('Create a new project')
-  .action(async (name) => {
-    const response = await prompt([
-      {
-        type: 'confirm',
-        name: 'useTypescript',
-        message: 'Use TypeScript?',
-        initial: false,
-      },
-    ]);
-
-    const { useTypescript } = response;
-
-    console.log(`Creating project '${name}'...`);
-
-    // Criar o diretório do projeto
-    fs.mkdirSync(name);
-    process.chdir(name);
-
-    // Estrutura de diretórios padrão
-    const folders = [
-      'app',
-      'app/Http',
-      'app/Models',
-      'config',
-      'database',
-      'public',
-      'resources',
-      'seeders',
-      'resources/views',
-      'routes',
-      'storage',
-      'storage/images',
-      'storage/files',
-    ];
-
-    folders.forEach(folder => {
-      fs.mkdirSync(folder, { recursive: true });
-    });
-
-    // Inicializar o projeto Node.js
-    execSync('npm init -y');
-
-    // Instalar dependências
-    const dependencies = useTypescript ? ['typescript'] : [];
-    execSync(`npm install --save-dev @babel/core @babel/preset-env ${dependencies.join(' ')}`);
-
-    // Configurar Babel
-    fs.writeFileSync('.babelrc', `{"presets": ["@babel/preset-env"]}`);
-
-    // Exemplo de arquivo index.js
-    fs.writeFileSync('index.js', '// Seu código aqui');
-
-    fs.writeFileSync('./config/config.js', 
-`module.exports = {
-  dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "123456",
-  database: "your-database",
-  define: {
-      timestamps: true,
-  },
-};`
-)
-
-    console.log('Project created successfully!');
-  });
+  .action(create);
 
 program
   .command('build')
