304d4546eb062f8165bac0e8053ea8f17ba62c95
Lucas-Jav <lucas.meialua@outlook.com>
2024-05-17 17:47:49 -0300

    :sparkles: feat(dependencies): add version in dependences

--- actions/create/dependencies.js
+++ actions/create/dependencies.js
@@ -3,26 +3,26 @@ const { execSync } = require('child_process');
 
 function installDependencies(useTypescript, useRateLimit) {
     const dependencies = [
-        "express", 
-        "pg", 
-        "pg-hstore", 
-        "body-parser",
-        "sequelize",
-        "dotenv",
-        "sqlite3",
-        "mysql2",
-        "ejs",
-        "morgan"
+        "express@4.19.2", 
+        "pg@8.11.5", 
+        "pg-hstore@2.3.4", 
+        "body-parser@1.20.2",
+        "sequelize@6.37.3",
+        "dotenv@16.4.5",
+        "sqlite3@5.1.7",
+        "mysql2@3.9.7",
+        "ejs@3.1.10",
+        "morgan@1.10.0"
     ];
 
     const devDependencies = [
-        'nodemon', 
-        'sequelize-cli'
+        'nodemon@3.1.0', 
+        'sequelize-cli@6.6.2'
     ];
 
     const globalDependencies = [
-        'nodemon', 
-        'sequelize-cli'
+        'nodemon@3.1.0', 
+        'sequelize-cli@6.6.2'
     ];  
 
     if (useTypescript) {
@@ -30,12 +30,18 @@ function installDependencies(useTypescript, useRateLimit) {
     }
 
     if (useRateLimit) {
-        dependencies.push("express-rate-limit");
+        dependencies.push("express-rate-limit@7.2.0");
     }
 
-
-    // Instalar dependências global
-    execSync(`npm install -g ${globalDependencies.join(' ')}`);
+    // Verificar e instalar dependências globais se necessário
+    globalDependencies.forEach(dep => {
+        try {
+            execSync(`npm list -g ${dep}`);
+        } catch (error) {
+            execSync(`npm install -g ${dep}`);
+            console.log(`${dep} installed globally.`);
+        }
+    });
 
     // Instalar dependências de produção
     execSync(`npm install ${dependencies.join(' ')}`);
